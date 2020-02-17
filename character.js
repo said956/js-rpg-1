14caeba09123ef863320a9631a155c5e21a62672
Safiye <safiye.akdede@hotmail.com>
2020-02-17 12:17:09 +0100

    reglage des bugs, jeu fonctionnelle

--- character.js
+++ character.js
@@ -5,18 +5,18 @@
         fighter2
     } from './gamelogic.js';
 
-    var healthPercentage1;
-    var healthPercentage2;
-    var fighter1Hit = document.getElementById("fighter01-hit");
-    var fighter1Heal = document.getElementById("fighter01-heal");
-    var fighter1Yield = document.getElementById("fighter01-yield");
-    var fighter2Hit = document.getElementById("fighter02-hit");
-    var fighter2Heal = document.getElementById("fighter02-heal");
-    var fighter2Yield = document.getElementById("fighter02-yield");
-    var personnage;
-    var action;
-    var result;
-    const movesLog = document.getElementById("starter");
+    let healthPercentage1;
+    let healthPercentage2;
+    let fighter1Hit = document.getElementById("fighter01-hit");
+    let fighter1Heal = document.getElementById("fighter01-heal");
+    let fighter1Yield = document.getElementById("fighter01-yield");
+    let fighter2Hit = document.getElementById("fighter02-hit");
+    let fighter2Heal = document.getElementById("fighter02-heal");
+    let fighter2Yield = document.getElementById("fighter02-yield");
+    let personnage;
+    let action;
+    let result;
+    const movesLog = document.getElementById("moves-log");
     let i = 1;
 
 
@@ -31,15 +31,15 @@
         this.maxDamage = 20;
         this.maxHealing = 30;
 
-        var fighter1Name = document.getElementById("name-rpg1").value;
-        var fighter2Name = document.getElementById("name-rpg2").value;
+        let fighter1Name = document.getElementById("name-rpg1").value;
+        let fighter2Name = document.getElementById("name-rpg2").value;
 
         //healing function
         this.heal = function() {
             if (event.target == document.getElementById("fighter01-heal")) {
-                var randomHealValue = Math.floor(Math.random() * (fighter1.maxHealing - fighter1.min + 1) + fighter1.min);
+                let randomHealValue = Math.floor(Math.random() * (fighter1.maxHealing - fighter1.min + 1) + fighter1.min);
                 //staff bonus
-                var healingValue;
+                let healingValue;
                 if (item == "Staff") {
                     healingValue = randomHealValue + Math.ceil(((randomHealValue * 20) / 100));
 
@@ -57,14 +57,14 @@
                 document.getElementById("healthbar1").innerHTML = fighter1.currenthealth;
                 document.getElementById("healthbar1").style.width = healthPercentage1 + "%";
 
-                moveAndLog(fighter1Name, "heals", healingValue);
+                afficherAction(fighter1Name, "is healing", healingValue);
 
                 fighter2Play();
 
             } else {
-                var randomHealValue = Math.floor(Math.random() * (fighter2.maxHealing - fighter2.min + 1) + fighter2.min);
+                let randomHealValue = Math.floor(Math.random() * (fighter2.maxHealing - fighter2.min + 1) + fighter2.min);
                 //staff bonus
-                var healingValue;
+                let healingValue;
                 if (item == "Staff") {
                     healingValue = randomHealValue + Math.ceil(((randomHealValue * 20) / 100));
                 } else {
@@ -81,7 +81,7 @@
                 document.getElementById("healthbar2").innerHTML = fighter2.currenthealth;
                 document.getElementById("healthbar2").style.width = healthPercentage2 + "%";
 
-                moveAndLog(fighter2Name, "heals", healingValue);
+                afficherAction(fighter2Name, "is healing", healingValue);
 
                 fighter1Play();
             }
@@ -90,7 +90,7 @@
 
         this.damage = function() {
             //nombre alea de degats
-            var chance = Math.random();
+            let chance = Math.random();
 
             if (event.target == document.getElementById("fighter01-hit")) {
 
@@ -106,11 +106,9 @@
 
                     document.getElementById("healthbar1").innerHTML = fighter1.currenthealth;
                     document.getElementById("healthbar1").style.width = healthPercentage1 + "%";
-
-                    console.log(moveAndLog);
                 }
 
-                var randomDamage = Math.floor(Math.random() * (fighter1.maxDamage - fighter1.min + 1) + fighter1.min);
+                let randomDamage = Math.floor(Math.random() * (fighter1.maxDamage - fighter1.min + 1) + fighter1.min);
 
 
                 switch (fighter1.item) {
@@ -137,19 +135,15 @@
                             }
                         }
                         break;
-                }
-                switch (fighter2.item) {
                     case "Boots":
                         if (chance < 0.3) {
                             randomDamage = 0;
-
                         }
                         break;
                 }
 
                 if (fighter2.race == "Humans") {
                     randomDamage -= Math.ceil(randomDamage * 0.2);
-
                     fighter2.currenthealth -= randomDamage;
                     healthPercentage2 = fighter2.currenthealth * (100 / fighter2.maxHealth)
                 } else if (fighter2.race != "Elves") {
@@ -177,20 +171,20 @@
                 document.getElementById("healthbar2").innerHTML = fighter2.currenthealth;
                 document.getElementById("healthbar2").style.width = healthPercentage2 + "%";
 
-                moveAndLog(fighter1Name, "hit", randomDamage);
+                afficherAction(fighter1Name, "is attacking", randomDamage);
 
                 if (fighter2.currenthealth <= 0) {
                     document.getElementById("healthbar2").innerHTML = "0";
                     document.getElementById("healthbar2").style.width = "0%";
 
-                    alert(fighter2Name + " is knocked out!\n" + fighter1Name + " wins this battle !");
+                    alert(fighter2Name + " is dead !" + fighter1Name + " wins this battle !");
                     location.reload();
                 }
 
                 fighter2Play();
 
             } else if (event.target == document.getElementById("fighter02-hit")) {
-                var randomDamage = Math.floor(Math.random() * (fighter2.maxDamage - fighter2.min + 1) + fighter2.min);
+                let randomDamage = Math.floor(Math.random() * (fighter2.maxDamage - fighter2.min + 1) + fighter2.min);
 
                 if (fighter2.race == "Vampires") {
                     fighter2.currenthealth = fighter2.currenthealth + Math.floor(fighter1.currenthealth * 0.1);
@@ -230,10 +224,7 @@
                             }
                         }
                         break;
-                }
-
                 //boots bonus
-                switch (fighter1.item) {
                     case "Boots":
                         if (chance < 0.3) {
                             randomDamage = 0;
@@ -241,53 +232,41 @@
                         }
                         break;
                 }
-
                 if (fighter1.race == "Humans") {
                     randomDamage -= Math.ceil(randomDamage * 0.2);
-
                     fighter1.currenthealth -= randomDamage;
                     healthPercentage1 = fighter1.currenthealth * (100 / fighter1.maxHealth)
                 } else if (fighter1.race != "Elves") {
                     fighter1.currenthealth -= randomDamage;
                     healthPercentage1 = fighter1.currenthealth * (100 / fighter1.maxHealth);
                 }
-
                 if (fighter1.race == "Elves") {
                     if (chance < 0.3) {
                         randomDamage = Math.ceil(randomDamage * 0.5);
-
                         fighter2.currenthealth -= randomDamage;
                         healthPercentage2 = fighter2.currenthealth * (100 / fighter2.maxHealth)
                         document.getElementById("healthbar2").innerHTML = fighter2.currenthealth;
                         document.getElementById("healthbar2").style.width = healthPercentage2 + "%";
-
                     } else {
                         fighter1.currenthealth -= randomDamage;
                         healthPercentage1 = fighter1.currenthealth * (100 / fighter1.maxHealth);
                         document.getElementById("healthbar1").innerHTML = fighter1.currenthealth;
                         document.getElementById("healthbar1").style.width = healthPercentage1 + "%";
-                        console.log("P1 curr health" + fighter1.currenthealth);
                     }
                 }
-
                 document.getElementById("healthbar1").innerHTML = fighter1.currenthealth;
                 document.getElementById("healthbar1").style.width = healthPercentage1 + "%";
-                moveAndLog(fighter2Name, "hit", randomDamage);
-
-                //check if player 1 hits 0 hp and pronounce winner is so
+                afficherAction(fighter2Name, "is attacking", randomDamage);
                 if (fighter1.currenthealth <= 0) {
                     document.getElementById("healthbar1").innerHTML = "0";
                     document.getElementById("healthbar1").style.width = "0%";
-
-                    alert(fighter1Name + " is knocked out !\n" + fighter2Name + " wins this battle !");
+                    alert(fighter1Name + " is dead !" + fighter2Name + " wins this battle !");
                     location.reload();
                 }
-
                 fighter1Play();
             }
         }
 
-        //disable functions to prevent cheating
         function fighter2Play() {
             fighter1Hit.disabled = true;
             fighter1Heal.disabled = true;
@@ -314,30 +293,29 @@
             }
         }
 
-        function moveAndLog(personnage, action, result) {
-
-            if (action == "Heal") {
-                movesLog.innerHTML += i + " " + personnage + " heals +" + result + ".<br>";
-            } else if (action == "Hit") {
-                movesLog.innerHTML += i + ". " + personnage + " " + action + ".<br>";
+        function afficherAction(personnage, action, result) {
+            if (action == "is healing") {
+                movesLog.innerHTML += `${i}. action of the battle, ${personnage} ${action} ${result} life points.<br>`;
+            } else if (action == "is attacking") {
+                movesLog.innerHTML += `${i}. action of the battle, ${personnage} ${action} ${result} damage.<br>`;
                 if (personnage == fighter1Name) {
                     personnage = fighter2Name;
                     if (result === 0) {
-                        movesLog.innerHTML += personnage + " dodged.<br>";
-                    } else if (fighter1.race != "vampires") {
-                        movesLog.innerHTML += personnage + " took -" + result + " damage.<br>";
-                    } else if (fighter1.race == "vampires") {
-                        movesLog.innerHTML += personnage + " took -" + result + " damage and lost extra 10.<br>";
+                        movesLog.innerHTML += `${personnage} dodged the attack of ${fighter1Name}.<br>`;
+                    } else if (fighter1.race != "Vampires") {
+                        movesLog.innerHTML += `${personnage} took -${result} damage.<br>`;
+                    } else if (fighter1.race == "Vampires") {
+                        movesLog.innerHTML += `${personnage} took -${result} damage + lost extra 10 life points.<br>`;
                     }
 
                 } else if (personnage == fighter2Name) {
                     personnage = fighter1Name;
                     if (result === 0) {
-                        movesLog.innerHTML += personnage + " dodged.<br>";
-                    } else if (fighter2.race != "vampires") {
-                        movesLog.innerHTML += personnage + " took -" + result + " damage.<br>";
-                    } else if (fighter2.race == "vampires") {
-                        movesLog.innerHTML += personnage + " took -" + result + " damage and lost extra 10.<br>";
+                        movesLog.innerHTML += `${personnage} dodged the attack of ${fighter2Name}.<br>`;
+                    } else if (fighter2.race != "Vampires") {
+                        movesLog.innerHTML += `${personnage} took -${result} damage.<br>`;
+                    } else if (fighter2.race == "Vampires") {
+                        movesLog.innerHTML += `${personnage} took -${result} damage + lost extra 10 life points.<br>`;
                     }
                 }
             }
--- gamelogic.js
+++ gamelogic.js
@@ -10,58 +10,33 @@
     let fighter1Healthbar;
     let fighter2Healthbar;
 
-    function yieldTheBattle() {
-        if (event.target == document.getElementById("fighter01-yield")) {
-            alert(fighter1Name + " yields" + "..." + fighter2Name + " wins this battle !");
-            document.location.reload();
-        } else if (event.target == document.getElementById("fighter02-yield")) {
-            alert(fighter2Name + " yields" + "..." + fighter1Name + " wins this battle !");
-            document.location.reload();
-        }
-
-    }
-
     document.getElementById("run").addEventListener("click", create);
 
     function create() {
         let fighter1Hit = document.getElementById("fighter01-hit");
         let fighter1Heal = document.getElementById("fighter01-heal");
-        let fighter1Yield = document.getElementById("fighter01-yiel");
+        let fighter1Yield = document.getElementById("fighter01-yield");
         let fighter2Hit = document.getElementById("fighter02-hit");
         let fighter2Heal = document.getElementById("fighter02-heal");
         let fighter2Yield = document.getElementById("fighter02-yield");
 
         fighter1Race = document.getElementById("race-rpg1").value;
         fighter1Item = document.getElementById("item-rpg1").value;
-        fighter2Race = document.getElementById("race-rpg1").value;
-        fighter2Item = document.getElementById("item-rpg1").value;
+        fighter2Race = document.getElementById("race-rpg2").value;
+        fighter2Item = document.getElementById("item-rpg2").value;
 
         fighter1 = new Person(fighter1Race, fighter1Item);
         fighter2 = new Person(fighter2Race, fighter2Item);
-        document.getElementById("race-rpg1").innerHTML += fighter1Race;
-        document.getElementById("item-rpg1").innerHTML += fighter1Item;
-        document.getElementById("race-rpg2").innerHTML += fighter2Race;
-        document.getElementById("item-rpg2").innerHTML += fighter2Item;
+        document.getElementById("fighter01-race").innerHTML += fighter1Race;
+        document.getElementById("fighter01-item").innerHTML += fighter1Item;
+        document.getElementById("fighter02-race").innerHTML += fighter2Race;
+        document.getElementById("fighter02-item").innerHTML += fighter2Item;
 
         fighter1Name = document.getElementById("name-rpg1").value;
         document.getElementById("fighter01-name").innerHTML += fighter1Name;
 
         fighter2Name = document.getElementById("name-rpg2").value;
         document.getElementById("fighter02-name").innerHTML += fighter2Name;
-
-        fighter1Hit.addEventListener("click", fighter1.damage);
-        fighter1Heal.addEventListener("click", fighter1.heal);
-        if(fighter1Yield){
-            fighter1Yield.addEventListener('click', yieldTheBattle, false);
-        }
-
-        document.getElementById("starter").addEventListener("click", starterFighter);
-
-        fighter2Hit.addEventListener("click", fighter2.damage);
-        fighter2Heal.addEventListener("click", fighter2.heal);
-        if(fighter2Yield){
-            fighter2Yield.addEventListener('click', yieldTheBattle, false);
-        }
 
         fighter1Hit.disabled = true;
         fighter1Heal.disabled = true;
@@ -74,88 +49,123 @@
             fighter2Yield.disabled = true;
         }
 
+        document.getElementById("starter").addEventListener("click", starterFighter);
+
+        fighter1Hit.addEventListener("click", fighter1.damage);
+        fighter1Heal.addEventListener("click", fighter1.heal);
+        if(fighter1Yield){
+            fighter1Yield.addEventListener('click', () => {
+                alert(fighter1Name + " yields." + fighter2Name + " wins this battle !");
+                document.location.reload();
+            });
+        }
+
+        fighter2Hit.addEventListener("click", fighter2.damage);
+        fighter2Heal.addEventListener("click", fighter2.heal);
+        if(fighter2Yield){
+            fighter2Yield.addEventListener('click', () =>{
+                alert(fighter2Name + " yields." + fighter1Name + " wins this battle !");
+                document.location.reload();
+            });
+        }
+
         switch (fighter1Race) {
             case "Humans":
                 document.getElementById("fighter01-raceimg").src = "./images/human.png";
+                document.getElementById("fighter01-raceimg").alt = "Human";
                 break;
             case "Elves":
                 document.getElementById("fighter01-raceimg").src = "./images/elf.png";
+                document.getElementById("fighter01-raceimg").alt = "Elf";
                 break;
             case "Vampires":
                 document.getElementById("fighter01-raceimg").src = "./images/vampire.png";
+                document.getElementById("fighter01-raceimg").alt = "Vampire";
+                break;
+            case "Orcs":
+                fighter1.maxHealth = 100 + ((100 * 40) / 100);
+                fighter1.currenthealth = 100 + ((100 * 40) / 100);
+                fighter1Healthbar = fighter1.currenthealth * (100 / fighter1.maxHealth);
+                document.getElementById("fighter01-raceimg").src = "./images/orc.png";
+                document.getElementById("fighter01-raceimg").alt = "Orc";
+                document.getElementById("fighter01-raceimg").style.transform = "scale(0.9)";
+                document.getElementById("fighter01-healthbar").style.width = "200px";
                 break;
         }
 
         switch (fighter2Race) {
             case "Humans":
                 document.getElementById("fighter02-raceimg").src = "./images/human.png";
+                document.getElementById("fighter02-raceimg").alt = "Human";
                 break;
             case "Elves":
                 document.getElementById("fighter02-raceimg").src = "./images/elf.png";
+                document.getElementById("fighter02-raceimg").alt = "Elf";
                 break;
             case "Vampires":
                 document.getElementById("fighter02-raceimg").src = "./images/vampire.png";
+                document.getElementById("fighter02-raceimg").alt = "Vampire";
+                break;
+            case "Orcs":
+                fighter2.maxHealth = 100 + ((100 * 40) / 100);
+                fighter2.currenthealth = 100 + ((100 * 40) / 100);
+                fighter2Healthbar = fighter2.currenthealth * (100 / fighter2.maxHealth);
+                document.getElementById("fighter02-raceimg").src = "./images/orc.png";
+                document.getElementById("fighter02-raceimg").alt = "Orc";
+                document.getElementById("fighter02-raceimg").style.transform = "scale(0.9)";
+                document.getElementById("fighter02-healthbar").style.width = "200px";
                 break;
         }
 
-        if (fighter1Race == "Orcs") {
-            fighter1.maxHealth = 100 + ((100 * 40) / 100);
-            fighter1.currenthealth = 100 + ((100 * 40) / 100);
-            fighter1Healthbar = fighter1.currenthealth * (100 / fighter1.maxHealth);
-            document.getElementById("fighter01-raceimg").src = "./images/orc.png";
-            document.getElementById("fighter01-healthbar").style.width = "100%";
-
-        } else {
-            fighter1.currenthealth = 100;
-            fighter1Healthbar = fighter1.currenthealth * (100 / fighter1.maxHealth)
-        }
-
-        if (fighter2Race == "Orcs") {
-            fighter2.maxHealth = 100 + ((100 * 40) / 100);
-            fighter2.currenthealth = 100 + ((100 * 40) / 100);
-            fighter2Healthbar = fighter2.currenthealth * (100 / fighter2.maxHealth);
-            document.getElementById("fighter01-raceimg").src = "./images/orc.png";
-            document.getElementById("fighter02-healthbar").style.width = "100%";
-
-        } else {
-            fighter2.currenthealth = 100;
-            fighter2Healthbar = fighter2.currenthealth * (100 / fighter2.maxHealth)
-        }
         document.getElementById("main-container").style.display = "none";
         document.getElementById("battle-container").style.display = "flex";
         document.getElementById("starter").style.display = "inherit";
         document.getElementById("healthbar1").innerHTML = fighter1.currenthealth;
         document.getElementById("healthbar2").innerHTML = fighter2.currenthealth;
-        document.getElementById("healthbar1").style.width = fighter1Healthbar + "%";
-        document.getElementById("healthbar2").style.width = fighter2Healthbar + "%";
+        document.getElementById("healthbar1").style.width = `fighter1Healthbar%`;
+        document.getElementById("healthbar2").style.width = `fighter2Healthbar%`;
 
         switch (fighter1Item) {
             case "Boots":
                 document.getElementById("fighter01-itemimg").src = "./images/boots.png";
+                document.getElementById("fighter01-itemimg").alt = "Boots";
                 break;
             case "Staff":
                 document.getElementById("fighter01-itemimg").src = "./images/staff.png";
+                document.getElementById("fighter01-itemimg").alt = "Staff";
+                document.getElementById("fighter01-itemimg").style.transform = "scale(2)";
                 break;
             case "Sword":
                 document.getElementById("fighter01-itemimg").src = "./images/sword.png";
+                document.getElementById("fighter01-itemimg").alt = "Sword";
+                document.getElementById("fighter01-itemimg").style.transform = "scale(2)";
                 break;
             case "Bow":
                 document.getElementById("fighter01-itemimg").src = "./images/bow.png";
+                document.getElementById("fighter01-itemimg").alt = "Bow";
+                document.getElementById("fighter01-itemimg").style.transform = "scale(1.3)";
                 break;
         }
 
         switch (fighter2Item) {
             case "Boots":
                 document.getElementById("fighter02-itemimg").src = "./images/boots.png";
+                document.getElementById("fighter02-itemimg").alt = "Boots";
                 break;
             case "Staff":
                 document.getElementById("fighter02-itemimg").src = "./images/staff.png";
+                document.getElementById("fighter02-itemimg").alt = "Staff";
+                document.getElementById("fighter02-itemimg").style.transform = "scale(2)";
                 break;
             case "Sword":
                 document.getElementById("fighter02-itemimg").src = "./images/sword.png";
+                document.getElementById("fighter02-itemimg").alt = "Sword";
+                document.getElementById("fighter02-itemimg").style.transform = "scale(2)";
                 break;
             case "Bow":
                 document.getElementById("fighter02-itemimg").src = "./images/bow.png";
+                document.getElementById("fighter02-itemimg").alt = "Bow";
+                document.getElementById("fighter02-itemimg").style.transform = "scale(1.3)";
                 break;
         }
 
@@ -175,9 +185,8 @@
                     fighter2Yield.disabled = false;
                 }
             }
-            document.getElementById("starter").innerHTML = "";
-            document.getElementById("starter").className = "moves-log";
-            document.getElementById("starter").removeEventListener("click", starterFighter);
+            document.getElementById("starter").style.display = "none";
+            document.getElementById("moves-log").style.display = "inherit";
         }
     }
 
